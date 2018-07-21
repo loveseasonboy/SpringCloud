4011948d3f5ae6c170473cf64437f8b523b72041
kdxf_hdhe <hdhe@iflytek.com>
2018-07-21 12:54:21 +0800

    重新构建一个eureka server

--- EurekaServer/src/main/java/com/EurekaServer.java
+++ EurekaServer/src/main/java/com/EurekaServer.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Server {
+public class EurekaServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Server.class, args);
+        SpringApplication.run(EurekaServer.class, args);
     }
 
 }
