0a151190cf6c6cc429708869edbf59274f6c63d4
kdxf_hdhe <hdhe@iflytek.com>
2018-07-26 18:09:40 +0800

    eureka remote feign  hystrix zuul 重试机制

--- CloudClient/src/main/java/com/config/RestTemplateConfig.java
+++ CloudClient/src/main/java/com/config/RestTemplateConfig.java
@@ -3,6 +3,7 @@ package com.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,6 +11,9 @@ public class RestTemplateConfig {
     @Bean
     @LoadBalanced
     RestTemplate restTemplate(){
+        HttpComponentsClientHttpRequestFactory httpRequestFactory=new HttpComponentsClientHttpRequestFactory();
+        httpRequestFactory.setReadTimeout(5000);
+        httpRequestFactory.setConnectTimeout(5000);
         return new RestTemplate();
     }
 }
